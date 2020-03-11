15c7ab91a1a53a90d18d2657f9f745a1f67b58f8
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-11 15:57:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,8 +35,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
-   # DB[:conn].execute(sql)
-    
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
